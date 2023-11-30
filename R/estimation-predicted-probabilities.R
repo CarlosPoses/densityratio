bd167d4d8e204d7a56613b5bcacf62244155b790
CarlosPoses <carlosgonzalezposes@gmail.com>
2023-11-30 10:24:42 +0100

    Update estimation-predicted-probabilities.R

--- R/estimation-predicted-probabilities.R
+++ R/estimation-predicted-probabilities.R
@@ -10,13 +10,19 @@
 #' @export
 #'
 #' @examples
-prob.to.dr <- function(data, probabilities, sample, id.num, id.den){
+prob.to.dr <- function(data,
+                       probabilities,
+                       sample, # variable identifier
+                       id.num, # sample
+                       id.den){ # sample
 
   # total n
   total.n <- nrow(data)
 
+  # Split data into numerator and denominator
   numerator <- data %>%
     filter({{ sample }} == id.num) # Naming numerator is appropriate?
+
   denominator <- data %>%
     filter({{ sample }} == id.den)
 
@@ -32,5 +38,26 @@ prob.to.dr <- function(data, probabilities, sample, id.num, id.den){
   den <- prior.den*(1-probabilities)
   dr <- (num/den)
 
+  return(dr)
+}
+
+#' Title
+#'
+#' @param model
+#' @param data
+#' @param sample
+#' @param id.num
+#' @param id.den
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dr.via.log.model <- function(model, data, sample = sample, id.num = "numerator", id.den = "denominator"){
+
+
+  data$predictions <- predict(model, type = "response", newdata = data)
+
+  dr <- prob.to.dr(data = data, probabilities = predictions, sample = sample, id.num = "numerator", id.den = "denominator")
   return(dr)
 }
